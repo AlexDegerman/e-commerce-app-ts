5fe5fc4092f5bc8ea956fe35cda789313e5ee51e
AlexDegerman <alex.degerman.dev@gmail.com>
2025-05-09 04:08:13 +0300

    Improve styling for product page

--- src/components/ProductPage.tsx
+++ src/components/ProductPage.tsx
@@ -8,7 +8,7 @@ import toys from "../data/toys.json";
 import books from "../data/books.json";
 import groceries from "../data/groceries.json";
 import "../styles/ProductPage.css";
-import { CheckCircle, ShoppingCart, Truck } from "lucide-react";
+import { CheckCircle, DollarSign, ShoppingCart, Truck } from "lucide-react";
 
 const allProducts = [
   ...electronics,
@@ -41,13 +41,19 @@ const ProductPage = () => {
 
 return (
     <div className="product-page-container">
-      <h1>{product.name}</h1>
-      <img src={product.image} alt={product.name} className="product-image" />
-      <p>Price: ${product.price}</p>
-      <p>{product.description}</p>
-      <p> <CheckCircle color="green" />Stock availability: {stockAmount} </p>
-      <p><Truck color="blue" />Estimated delivery: {formattedDate} </p>
-      <p><ShoppingCart/>Add to cart</p>
+      <h1 className="product-title">{product.name}</h1>
+      <img src={product.image} alt={product.name} className="product-page-image" />
+      <div className="product-details">
+        <div className="product-description">
+          <p>{product.description}</p>
+        </div>
+        <div className="product-info">
+          <p><DollarSign color="green" size="20px"/><strong>Price: ${product.price}</strong></p>
+          <p> <CheckCircle color="green" size="20px"/>Stock availability: {stockAmount} </p>
+          <p><Truck color="blue" size="20px"/>Estimated delivery: {formattedDate} </p>
+          <p><ShoppingCart color="#5a2ca0" size="20px"/><button className="add-to-cart-button">Add to cart</button></p>
+        </div>
+      </div>
     </div>
   )
 }
